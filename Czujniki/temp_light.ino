84d062862aa939491b37f37b772d9a0bc5d55b33
arudim <adalbertmid@gmail.com>
2024-12-02 09:38:09 +0100

    czujniki swiatla i temp

--- Czujniki/temp_light.ino
+++ Czujniki/temp_light.ino
@@ -4,14 +4,13 @@
 
 BH1750 lightMeter;
 DHT dht(26,DHT11);
+
 void setup() {
   dht.begin();
   Serial.begin(9600);
 
-
   Wire.begin(); //I2C comunication SCL pin D22 SDA pin D21
  
-
   lightMeter.begin();
 
   Serial.println(F("BH1750 Test begin"));
@@ -19,9 +18,11 @@ void setup() {
 
 void loop() {
   float lux = lightMeter.readLightLevel();
-  
-  Serial.print("Light: ");
-  Serial.print(lux);
-  Serial.println(" lx");
+  float temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  Serial.printf("Temperatura: %.1f \n", temp);
+  Serial.printf("Wilgotnosc: %.1f \n", humidity);
+  Serial.printf("Naswietlenie: %.2f \n", lux);
   delay(1000);
 }
